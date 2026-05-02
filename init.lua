64ce16ff269e768e0a9aed6c922cedb21d3c14c2
Ilzumi Zeminori <72821250+zeminori@users.noreply.github.com>
2026-05-02 22:08:43 +0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,36 +1,78 @@
-local fstorturing = {}
+fstorturing_interval = 0.1
 
-local torture_fs = {"size[20,12]"}
+fstorturing = {}
+
+local lagfs = {"size[20,12]"}
 for x=0, 20, 0.1 do
 	for y=0, 12, 0.1 do
-		table.insert(torture_fs, string.format("label[%s,%s;::]", x, y))
+		table.insert(lagfs, string.format("label[%s,%s;::]", x, y))
 	end
 end
-torture_fs = table.concat(torture_fs)
+lagfs = table.concat(lagfs)
 
-core.register_chatcommand("fstorture",{
+local flashfs = "size[160,90]background[0,0;160,90;\\[fill:160x90:#FFF]"
+
+core.register_chatcommand("lagfst",{
 	description = "Torture player using laggy formspec",
 	privs = {server=true},
 	func = function(name, param)
+		if not param or param == "" then return false, "Missing param" end
 		if fstorturing[param] then
 			fstorturing[param] = nil
 			return true, "Torturing disabled for "..param
 		else
 			if core.get_player_by_name(param) then
-				core.show_formspec(param, "fstorture", torture_fs)
+				core.show_formspec(param, "fstorture", lagfs)
 			end
-			fstorturing[param] = true
+			fstorturing[param] = "lagfs"
 			return true, "Torturing enabled for "..param
 		end
 end})
+
+core.register_chatcommand("flashfst",{
+	description = "Torture player using flashy formspec",
+	privs = {server=true},
+	func = function(name, param)
+		if not param or param == "" then return false, "Missing param" end
+		if fstorturing[param] then
+			fstorturing[param] = nil
+			return true, "Torturing disabled for "..param
+		else
+			if core.get_player_by_name(param) then
+				core.show_formspec(param, "fstorture", flashfs)
+			end
+			fstorturing[param] = "flashfs"
+			return true, "Torturing enabled for "..param
+		end
+end})
+
+local timer = 0
+local opened
+core.register_globalstep(function(dtime)
+	timer = timer + dtime
+	if timer >= fstorturing_interval then
+		for pname, ttype in pairs(fstorturing) do
+			if ttype == "flashfs" and core.get_player_by_name(pname) then
+				if opened then
+					core.close_formspec(pname, "fstorturing")
+					opened = nil
+				else
+					core.show_formspec(pname, "fstorturing", flashfs)
+					opened = true
+				end
+			end
+		end
+		timer = 0
+	end
+end)
 
 core.register_chatcommand("fstorturing",{
 	description = "View torturing players list",
 	privs = {server=true},
 	func = function(name, param)
 		local out = {}
-		for pname,_ in pairs(fstorturing) do
-			table.insert(out, pname)
+		for pname, ttype in pairs(fstorturing) do
+			table.insert(out, string.format("%s(%s)", pname, ttype))
 		end
 		table.sort(out)
 		return true, "Torturing players: "..table.concat(out, ", ")
@@ -39,15 +81,15 @@ end})
 core.register_on_player_receive_fields(function(player, fname, fields)
 	if fname == "fstorture" then
 		local name = player and player:get_player_name()
-		if name and fstorturing[name] then
-			core.show_formspec(name, "fstorture", torture_fs)
+		if name and fstorturing[name] == "lagfs" then
+			core.show_formspec(name, "fstorture", lagfs)
 		end
 	end
 end)
 
 core.register_on_joinplayer(function(player)
 	local name = player and player:get_player_name()
-	if fstorturing[name] then
-		core.show_formspec(name, "fstorture", torture_fs)
+	if fstorturing[name] == "lagfs" then
+		core.show_formspec(name, "fstorture", lagfs)
 	end
 end)
